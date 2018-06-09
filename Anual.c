46e712a9f6e94b6ac1a23c7973e8a4decbed83a8
Synaxis <oiacow@gmail.com>
2018-06-09 06:12:06 -0300

    Arrumando erros de Definição de tipo

--- Anual.c
+++ Anual.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 //Values
-float comidaSemana;
+double   comidaSemana;
 float luzMes;
 float conducaoSemana;
 float aguaMes;
@@ -12,10 +12,11 @@ float resultado;
 int main() {
    puts("================================================\n");
    printf("Olá ,vamos calcular uma média do seu gasto anual\n");
+   puts("Instruções -> Nao use , . Apenas .  \n");
    puts("===============================================\n");
    printf("Insira quanto você gasta por SEMANA nas compras do mercado E alimentação\n");
-   scanf("%f", &comidaSemana);
-   printf("Voce digitou %f\n", comidaSemana);
+   scanf("%lf", &comidaSemana);
+   //printf("Voce digitou: %.2f\n", comidaSemana);
    
    return 0;
    
